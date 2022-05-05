aab95116aa95e1d5fc5e5569b43f3bf6fa56d62d
mexiCAN2019 <45108641+mexiCAN2019@users.noreply.github.com>
2022-05-05 15:42:17 -0500

    Added get weeks, post week, and delete week requests

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/years/:year', (req,res,next) => {
         if(err){
             next(err);
         } else{
-            res.status(201).json({months: rows});
+            res.status(200).json({months: rows});
         }
     });
 });
@@ -156,10 +156,113 @@ app.delete(`/years/:year`, (req,res,next) => {
         if(err){
             next(err);
         } else{
-            res.sendStatus(204);
+            res.sendStatus(200);
         }
     })
-})
+});
+
+// weeksRouter.get('/', (req,res,next) => {
+//     const weeks = req.query;
+//     console.log(weeks.monthId);
+//     db.all(`SELECT * FROM Weeks WHERE yearFK = $yearFK AND monthId = $monthId;`, {
+//         $yearFK: weeks.year,
+//         $monthId: weeks.monthId
+//     }, (err, rows) => {
+//         if(err) {
+//             next(err);
+//         } else {
+//             res.status(200).send({weeks: rows});
+//         }
+//     });
+// });
+
+app.get('/years/:year/month', (req,res,next) => {
+    db.query(`SELECT * FROM weeks WHERE year = ${req.params.year} AND months_id = ${req.query.monthID};`, (err, rows) => {
+        if(err){
+            next(err);
+        } else{
+            res.status(200).json({weeks: rows});
+        }
+    })
+});
+
+// weeksRouter.post('/', (req,res,next) => {
+//     const newWeek = req.query;
+//     db.run(`INSERT INTO Weeks (week, yearFK, monthId)
+//         VALUES ($week, $yearFK, $monthId);`, {
+//             $week: newWeek.week,
+//             $yearFK: newWeek.yearFK,
+//             $monthId: newWeek.monthId
+//         }, function(err) {
+//             if(err) {
+//                 next(err);
+//             } else {
+//                 db.get(`SELECT * FROM Weeks WHERE id = $id;`, {$id: this.lastID}, (err, row) => {
+//                     if(err) {
+//                         next(err);
+//                     } else{
+//                         res.status(201).json({week: row});
+//                     }
+//                 });
+//             }
+//         });
+// });
+
+app.post('/years/:year/month', (req,res,next) => {
+    const newWeek = req.body.week;
+    if(!req.params.year || !newWeek.date || !newWeek.monthID) {
+        res.sendStatus(400);
+    } else{
+        db.query(`INSERT INTO weeks SET ?;`, {
+            week: newWeek.date,
+            year: req.params.year,
+            months_id: newWeek.monthID
+        }, (err,row) => {
+            if(err){
+                next(err);
+            } else{
+                res.sendStatus(201);
+            }
+        })
+    }
+});
+
+// weeksRouter.delete('/', (req,res,next) => {
+//     db.all(`SELECT * FROM TimeLogger WHERE weekId = $weekId;`, {$weekId: req.query.weekId}, (err, rows) => {
+//         if(err) {
+//             next(err);
+//         } else if(rows.length){
+//             res.sendStatus(400);
+//         } else {
+//             db.all(`SELECT * FROM Checkbox WHERE weekId = $weekId;`, {$weekId: req.query.weekId}, (err, rows) => {
+//                 if(rows.length){
+//                     res.sendStatus(400);
+//                 } else{
+//                     db.all(`SELECT * FROM Subjective WHERE weekId = $weekId;`, {$weekId: req.query.weekId}, (err, rows) => {
+//                         if(rows.length){
+//                             res.sendStatus(400);
+//                         } else{
+//                             db.run(`DELETE FROM Weeks WHERE id = $id;`, {$id: req.query.weekId}, (err) => {
+//                                     res.sendStatus(200);
+//                             });
+//                         }
+//                     });
+//                 }
+//             });
+//         }
+//     });
+// });
+// for some reason, even though rows returns an empty array when appropriate, else if statement runs the sendStatus(400). But issue resolved when adding .length to rows.          
+
+app.delete('/years/:year/month', (req,res,next) => {
+    db.query(`DELETE FROM weeks WHERE id = ${req.body.weekID};`, (err) => {
+        if(err){
+            next(err);
+        } else{
+            res.sendStatus(200);
+        }
+    })
+});
 
 
 app.use(errorHandler());
